3795afd2e0fb7fcbe45e23d42356c9d8a17bfcaa
Diego Pessoa <diegopessoa12@gmail.com>
2023-05-05 19:16:53 -0300

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,37 @@
+export const GITHUB_EDIT_URL = 'https://github.com/ifpb/lm/tree/main';
+
+export const CLASSNOTE_SLUGS = [
+  // 'web-server/live-server',
+  'markup/markup-language',
+  'html/introduction-html',
+  'css/introduction-css',
+  // 'css/cascade-inheritance',
+  'css/text-style',
+  'css/web-fonts',
+  'css/icons',
+  'css/box-model',
+  'css/table',
+  'css/background',
+  'css/flexbox-latyout',
+  'css/grid-layout',
+  'html/web-resource',
+  'web-server/deploy-pages',
+  'css/responsive-design',
+  'css/form',
+  'css/position',
+  'css/transform',
+  'css/transition',
+  'css/animation',
+  'library/introduction-bootstrap',
+];
+
+export const MENU = [
+  {
+    title: 'Disciplina',
+    url: '/lm/',
+  },
+  {
+    title: 'Conteúdos',
+    url: '/lm/classnotes/',
+  },
+];
